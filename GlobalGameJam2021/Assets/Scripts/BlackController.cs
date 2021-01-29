fe50158ca78775aeae24eb3bf7a1243862c8087c
M2neko <bwwang@ucdavis.edu>
2021-01-30 02:33:31 +0800

    Update Scene1

--- GlobalGameJam2021/Assets/Scripts/BlackController.cs
+++ GlobalGameJam2021/Assets/Scripts/BlackController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BlackController : MonoBehaviour
 {
+    [SerializeField] private float Darklast = 0.5f;
+    [SerializeField] private float Brightlast = 3.0f;
+    private float time = 0.0f;
+    private float Darktime = 0.0f;
+    public bool isDark = false;
+    // Start the game?
+    public bool startGame = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,38 @@ public class BlackController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDark)
+        {
+            Darktime += Time.deltaTime;
+            if (Darktime >= Darklast)
+            {
+                isDark = false;
+                time = 0.0f;
+                Darktime = 0.0f;
+            }
+        }
+        else
+        {
+            time += Time.deltaTime;
+            if (time >= Brightlast)
+            {
+                // Sound
+                gameObject.GetComponent<AudioSource>().Play();
+                isDark = true;
+                time = 0.0f;
+                Darktime = 0.0f;
+            }
+        }
+
+
+
+        if (isDark)
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 }
