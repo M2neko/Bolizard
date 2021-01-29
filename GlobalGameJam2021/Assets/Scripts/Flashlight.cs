f78acf5c51905d6e167177b6dd0a6bbd778e3540
Zheng Wang <tzwwang@ucdavis.edu>
2021-01-30 07:57:59 +0800

    new update

--- GlobalGameJam2021/Assets/Scripts/Flashlight.cs
+++ GlobalGameJam2021/Assets/Scripts/Flashlight.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         BoyItemManager itemManager = collision.GetComponent<BoyItemManager>();
